31098039aae678e9ad8e4527cd9935af6f465dc4
corletey <corleteya@gmail.com>
2025-02-27 16:49:23 +0000

    hero image rectification

--- assets/js/main.js
+++ assets/js/main.js
@@ -170,11 +170,11 @@
 /* 10. WOW active */
     new WOW().init();
 
-// 11. ---- Mailchimp js --------//  
-    function mailChimp() {
-      $('#mc_embed_signup').find('form').ajaxChimp();
-    }
-    mailChimp();
+// // 11. ---- Mailchimp js --------//  
+//     function mailChimp() {
+//       $('#mc_embed_signup').find('form').ajaxChimp();
+//     }
+//     mailChimp();
 
 
 // 12 Pop Up Img
